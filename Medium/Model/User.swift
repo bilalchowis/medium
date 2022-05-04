ea351faa679956a3a3a3dcb0b45faaa7033d8661
Bilal <bilal@chowis.com>
2022-05-04 23:49:20 +0300

    Create user model

---
+++ Medium/Model/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  Medium
+//
+//  Created by Bilal Bakhrom on 04/05/2022.
+//
+
+import Foundation
+
+struct User: Codable, Identifiable {
+    var id: String
+    var firstName: String
+    var lastName: String
+    var imagePath: String?
+    var postIDs: [String]
+}
