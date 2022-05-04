58d3f84dc841ce89ca442dcd41175772e2010d97
Bilal <bilal@chowis.com>
2022-05-04 23:22:38 +0300

    Re arrange folders and files

---
+++ Medium/Modules/Main/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  Medium
+//
+//  Created by Bilal Bakhrom on 04/05/2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+    }
+}
+
